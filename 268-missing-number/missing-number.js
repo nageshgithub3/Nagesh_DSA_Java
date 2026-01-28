52f4ba48954c46296e02fb92630cf02b958c40a4
Rangaswamy Gari Nagesh <142003241+nageshgithub3@users.noreply.github.com>
2026-01-28 10:32:32 +0530

    Time: 0 ms (100.00%) | Memory: 56.1 MB (77.05%) - LeetSync

---
+++ 268-missing-number/missing-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+
+    let n=nums.length;
+
+    let expectedSum = n*(n+1)/2;
+    let actualSum = 0;
+
+    for(let i=0;i<n;i++){
+        actualSum += nums[i];
+    }
+
+    return expectedSum - actualSum;
+    
+};
