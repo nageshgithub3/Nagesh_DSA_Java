f1936ddc6582d02e9a7cddbca04b94d5224f43d0
Rangaswamy Gari Nagesh <142003241+nageshgithub3@users.noreply.github.com>
2026-02-02 12:34:35 +0530

    Time: 12 ms (32.36%) | Memory: 86 MB (10.76%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+
+    let arr = [];
+
+    let curr = head;
+
+    while(curr!=null){
+        arr.push(curr.val);
+        curr=curr.next;
+    }
+
+    let left = 0;
+    let right = arr.length-1;
+
+    while(left < right){
+        if(arr[left]!==arr[right]) return false;
+        left++;
+        right--;
+    }
+    return true;
+    
+};
