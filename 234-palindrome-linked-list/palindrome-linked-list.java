1e1ecde49294c80af7c1192c44d8a89b148c7e37
Rangaswamy Gari Nagesh <142003241+nageshgithub3@users.noreply.github.com>
2026-02-03 10:28:17 +0530

    Time: 7 ms (33.94%) | Memory: 101.7 MB (10.45%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -17,14 +17,16 @@ class Solution {
 
         while(curr!=null){
             list.add(curr.val);
-            curr=curr.next;
+            curr = curr.next;
         }
 
         int left = 0;
         int right = list.size()-1;
 
         while(left < right){
-            if(!list.get(left).equals(list.get(right))) return false;
+            if(!list.get(left).equals(list.get(right))){
+                return false;
+            }
             left++;
             right--;
         }
