ad9ac2cdb704818c24098325f1e1c298c39c137d
Rangaswamy Gari Nagesh <142003241+nageshgithub3@users.noreply.github.com>
2026-02-06 11:20:54 +0530

    Time: 58 ms (89.78%) | Memory: 69.9 MB (7.78%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
@@ -12,21 +12,20 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function(headA, headB) {
-// create a set
-    let set = new Set();
 
-    
-// all the headB added into the set
+    let store = new Set();
+
     while(headB){
-        set.add(headB);
+        store.add(headB);
         headB=headB.next;
-    }   
-// check headA present in the set return the element 
+    }
+
     while(headA){
-        if(set.has(headA)){
+        if(store.has(headA)){
             return headA;
         }
-        headA=headA.next;
+        headA=headA.next
     }
-    return null;  
+    return null;
+    
 };
