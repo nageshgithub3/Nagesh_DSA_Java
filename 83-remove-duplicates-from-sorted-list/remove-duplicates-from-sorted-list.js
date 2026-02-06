c2e1cf2baf55f7c1949ef00d6f4148bbdbde6dd3
Rangaswamy Gari Nagesh <142003241+nageshgithub3@users.noreply.github.com>
2026-02-06 11:46:35 +0530

    Time: 1 ms (38.69%) | Memory: 58.5 MB (13.52%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
@@ -15,10 +15,9 @@ var deleteDuplicates = function(head) {
 
     while(curr&&curr.next){
         if(curr.val===curr.next.val){
-            curr.next=curr.next.next;
+            curr.next = curr.next.next;
         }else{
-
-            curr=curr.next;
+            curr = curr.next;
         }
     }
     return head;
