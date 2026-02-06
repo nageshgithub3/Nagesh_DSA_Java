0222a54b24346f1316a6416ab274b7921e2312a9
Rangaswamy Gari Nagesh <142003241+nageshgithub3@users.noreply.github.com>
2026-02-06 11:40:26 +0530

    Time: 0 ms (100.00%) | Memory: 54.9 MB (47.94%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
@@ -11,15 +11,12 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-    /* two pass solution
-// create a dummy node for deleting a starting index
+
     let sentinel = new ListNode();
     sentinel.next = head;
 
-    // calculate linkedlist length;
-
     let length=0;
-    while(head){
+    while(head!=null){
         head=head.next;
         length++;
     }
@@ -27,30 +24,11 @@ var removeNthFromEnd = function(head, n) {
     let prevPos = length - n;
 
     let prev = sentinel;
-    for(let i=0; i<prevPos;i++){
+
+    for(let i=0;i<prevPos;i++){
         prev=prev.next;
     }
-    prev.next=prev.next.next;
-    return sentinel.next;*/
-
-    let sentinel = new ListNode();
-    sentinel.next= head;
-
-    let fast = sentinel;
-    let slow = sentinel;
-
-    for(let i=0;i<=n;i++){
-        fast=fast.next;
-    }
-
-    while(fast!=null){
-        fast=fast.next;
-        slow=slow.next;
-    }
-    slow.next = slow.next.next;
-
+    prev.next=prev.next.next; // golden statement for delete
     return sentinel.next;
-
-   
     
 };
