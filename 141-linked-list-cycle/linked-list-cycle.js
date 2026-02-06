f259507726ca8f9b89e6f65f3caf583325e71f4e
Rangaswamy Gari Nagesh <142003241+nageshgithub3@users.noreply.github.com>
2026-02-06 11:05:01 +0530

    Time: 48 ms (85.42%) | Memory: 60.1 MB (13.64%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.js
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -12,15 +12,18 @@
  */
 var hasCycle = function(head) {
 
-    let slow=fast=head;
+    if(!head) return false;
+    let slow=head;
+    let fast=head.next;
 
-    while(fast&&fast.next){
+    while(slow!=fast){
+        if(fast===null||fast.next===null){
+            return false;
+            
+        }
         slow=slow.next;
         fast=fast.next.next;
-        if(slow===fast){
-            return true;
-        }
     }
-    return false;
+    return true;
     
 };
