122bdba03ac2bd6726f98bd8edf5a3e767994748
Rangaswamy Gari Nagesh <142003241+nageshgithub3@users.noreply.github.com>
2026-02-07 14:25:34 +0530

    Time: 42 ms (86.94%) | Memory: 57.3 MB (69.63%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    let slow=fast=head;
+
+    while(fast&&fast.next){
+        slow=slow.next;
+        fast=fast.next.next;
+        if(slow==fast){
+            let p1 = head;
+            let p2 = slow;
+
+            while(p1!==p2){
+                p1 = p1.next;
+                p2 = p2.next;
+            }
+            return p1;
+        }
+    }
+    return null;
+    
+};
