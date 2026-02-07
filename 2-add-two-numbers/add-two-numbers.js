133c2c34985aff2a80256a6ad763ea361954bb29
Rangaswamy Gari Nagesh <142003241+nageshgithub3@users.noreply.github.com>
2026-02-07 11:00:14 +0530

    Time: 2 ms (86.13%) | Memory: 60.8 MB (92.30%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.js
+++ 2-add-two-numbers/add-two-numbers.js
@@ -13,20 +13,20 @@
 var addTwoNumbers = function(l1, l2) {
 
     let ans = new ListNode();
-    let ansCopy = ans
-    let carry = 0;
+    let ansCopy=ans;
+    let carry = 0; // 1 // 1
     while(l1||l2||carry){
-        let sum = (!l1?0:l1.val)+(!l2?0:l2.val)+carry;
+        let sum = (!l1?0:l1.val) + (!l2?0:l2.val)+carry;
         carry = Math.floor(sum/10);
-        let digit = sum % 10;
+        let digit = sum%10;
 
-        let newNode = new ListNode(digit)
+        let newNode = new ListNode(digit);
+
         ans.next=newNode;
-        ans = ans.next;
+        ans=ans.next;
+
         l1 = l1&&l1.next;
         l2 = l2&&l2.next;
     }
     return ansCopy.next;
-   
-    
 };
