73d4cca21a9d1a244edc4152b0303bbace90482d
Rangaswamy Gari Nagesh <142003241+nageshgithub3@users.noreply.github.com>
2026-02-07 12:31:47 +0530

    Time: 0 ms (100.00%) | Memory: 58 MB (33.69%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -11,20 +11,29 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-
+/*
     if(!l1) return l2;
     if(!l2) return l1;
 
-    let curr = null;
+    // find the first current node
+
     if(l1.val < l2.val){
         curr = l1;
         l1=l1.next;
     }else{
         curr = l2;
         l2=l2.next;
-    }
+    }*/
 
-    let start = curr;
+
+    let start = new ListNode();
+
+
+    // let us assume start = curr node.
+
+     let curr = start;
+
+    // loop runs l1!=null && l2=!=null
 
     while(l1&&l2){
 
@@ -38,13 +47,16 @@ var mergeTwoLists = function(l1, l2) {
         curr=curr.next;
 
     }
+
+    // if l1 not exit return curr.next=l2
     if(!l1){
         curr.next=l2;
     }
+    // if l2 not exit return curr.next=l1
     if(!l2){
         curr.next=l1;
     }
 
-    return start;
+    return start.next;
     
 };
