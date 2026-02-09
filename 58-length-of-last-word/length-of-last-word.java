bdfc5a3a466e685c825c14082c12986b7008ac8d
Rangaswamy Gari Nagesh <142003241+nageshgithub3@users.noreply.github.com>
2026-02-09 08:00:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (83.16%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -4,14 +4,27 @@ class Solution {
         int n = s.length()-1;
 
         while(n>=0){
+            if(s.charAt(n)==' '){
+                --n;
+            }else{
+                break;
+            }
+            /*
             if(s.charAt(n)!=' ')break;
-            --n;
+            --n;*/
         }
         int count=0;
         while(n>=0){
+            if(s.charAt(n)!=' '){
+                --n;
+                count++;
+            }else{
+                break;
+            }
+            /*
             if(s.charAt(n)==' ')break;
             --n;
-            count++;
+            count++;*/
         }
         return count;
         
