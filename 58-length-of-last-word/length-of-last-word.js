6cb523e5ac9b37eed5b07591d3dd438d3c7ff558
Rangaswamy Gari Nagesh <142003241+nageshgithub3@users.noreply.github.com>
2026-02-09 07:51:19 +0530

    Time: 0 ms (100.00%) | Memory: 53.2 MB (86.55%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    // trim all the spaces at the end
+    let n = s.length-1;
+
+    while(n>=0){
+        if(s[n]===" "){
+            --n;
+        }else{
+            break;
+        }
+    }
+    
+
+    // count all the character till you reach a space
+    let count = 0;
+    while(n>=0){
+        if(s[n]!=" "){
+            --n;
+            count++;
+        }else{
+            break;
+        }
+    }
+    return count;
+    
+};
