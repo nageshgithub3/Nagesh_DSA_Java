df9691d0252c3b27b970c647797c2102c5dcd7fa
Rangaswamy Gari Nagesh <142003241+nageshgithub3@users.noreply.github.com>
2026-02-10 10:41:04 +0530

    Time: 2 ms (13.72%) | Memory: 54.3 MB (53.91%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.js
+++ 782-jewels-and-stones/jewels-and-stones.js
@@ -8,9 +8,16 @@ var numJewelsInStones = function(jewels, stones) {
     let count = 0;
 
     for(let i=0;i<stones.length;i++){
-        if(jewels.includes(stones[i])){
-            count++;
+
+        for(let j=0;j<jewels.length;j++){
+            if(jewels[j]===stones[i]){
+                ++count;
+                break;
+            }
         }
+        // if(jewels.includes(stones[i])){
+        //     count++;
+        // }
     }
     return count;
     
