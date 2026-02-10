793dfd264ec6e6ab0cc79d850884ce8beb4ab561
Rangaswamy Gari Nagesh <142003241+nageshgithub3@users.noreply.github.com>
2026-02-10 10:10:51 +0530

    Time: 1 ms (82.91%) | Memory: 57.2 MB (84.73%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.js
+++ 3194-find-words-containing-character/find-words-containing-character.js
@@ -8,9 +8,21 @@ var findWordsContaining = function(words, x) {
     let res = [];
 
     for(let i=0;i<words.length;i++){
-        if(words[i].includes(x)){
-            res.push(i);
-        }
+        // find char x present in the word or not
+
+            for(let j=0;j<words[i].length;j++){
+                if(words[i][j]==x){
+                    res.push(i);
+                    break;
+                }
+            }
+
+
+
+
+        // if(words[i].includes(x)){
+        //     res.push(i);
+        // }
     }
     return res;
     
