a43553a05fa5e4d4b432d7b0eecbee3a2ccc08df
Rangaswamy Gari Nagesh <142003241+nageshgithub3@users.noreply.github.com>
2026-02-13 10:33:41 +0530

    Time: 9 ms (23.03%) | Memory: 63 MB (11.45%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -9,7 +9,14 @@ var isPalindrome = function(s) {
     let rev="";
 
     for(let i=0;i<s.length;i++){
-        if(s[i].match(/[a-z0-9]/i)){
+
+
+        if((s[i].charCodeAt() >= "a".charCodeAt() && s[i].charCodeAt() <= "z".charCodeAt()) ||
+           (s[i].charCodeAt() >= "0".charCodeAt() && s[i].charCodeAt() <= "9".charCodeAt())){
+
+
+
+        // if(s[i].match(/[a-z0-9]/i)){
             filteredString += s[i];
             rev = s[i] + rev;
         }
