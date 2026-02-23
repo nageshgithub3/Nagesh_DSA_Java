fbf14e9ea4594bfb0b3a9aa89af45e6f73bb5b74
Rangaswamy Gari Nagesh <142003241+nageshgithub3@users.noreply.github.com>
2026-02-23 11:42:08 +0530

    Time: 0 ms (100.00%) | Memory: 55.3 MB (5.14%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -3,19 +3,30 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
+/*
+    s=s.trim();
+    s=s.split(" ");
+    return s[s.length-1].length;
+    */
 
-        let n = s.length-1;
-        let count = 0;
-
-        while(n>=0){
-            if(s[n]!=" "){
-                ++count;
-            }else if(count > 0){
-                break;
-            }
+    let n = s.length-1;
+    while(n>=0){
+        if(s[n]===" "){
             --n;
+        }else{
+            break;
         }
-        return count;
-    
+    }
+
+    let count=0;
+    while(n>=0){
+        if(s[n]!=" "){
+            --n;
+            ++count;
+        }else{
+            break;
+        }
+    }
+    return count;
     
 };
