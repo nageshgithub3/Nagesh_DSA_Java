221974d1c3b46a2e867a4306ae3ecb166b987dda
Rangaswamy Gari Nagesh <142003241+nageshgithub3@users.noreply.github.com>
2026-02-25 07:56:37 +0530

    Time: 12 ms (70.72%) | Memory: 55.5 MB (78.30%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,7 +4,27 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
+    if(s.length !== t.length){
+        return false;
+    }
 
-    return s.split("").sort().join("")===t.split("").sort().join("");
+    let map={};
+
+    for(let i=0;i<s.length;i++){
+        if(!map[s[i]]){
+            map[s[i]]=1;
+        }else{
+            ++map[s[i]];
+        }
+    }
+
+    for(let i=0;i<t.length;i++){
+        if(!map[t[i]] || map[t[i]]<0){
+            return false;
+        }else{
+            --map[t[i]];
+        }
+    }
+    return true;
     
 };
