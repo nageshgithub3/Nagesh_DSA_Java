23ace146f8e66dd12b98bb15852bd81b435306e4
Rangaswamy Gari Nagesh <142003241+nageshgithub3@users.noreply.github.com>
2026-03-03 21:44:56 +0530

    Time: 2 ms (83.84%) | Memory: 55.7 MB (58.82%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -6,13 +6,20 @@ var isValid = function(s) {
 
     let stack = [];
 
+    let map = {
+        "{":"}",
+        "[":"]",
+        "(":")"
+    }
+
     for(let i=0;i<s.length;i++){
-        if(s[i] === "{" ||s[i] === "[" || s[i] === "("){
+        if(map[s[i]]){
+        //if(s[i] === "{" ||s[i] === "[" || s[i] === "("){
             stack.push(s[i]);
         }else{
             let top = stack.pop();
-
-            if(!top || (top === "{" && s[i] != "}")||(top === "[" && s[i] != "]")||(top === "(" && s[i] != ")")){
+            if(!top || s[i] != map[top]){
+            //if(!top || (top === "{" && s[i] != "}")||(top === "[" && s[i] != "]")||(top === "(" && s[i] != ")")){
                 return false
             }
         }
