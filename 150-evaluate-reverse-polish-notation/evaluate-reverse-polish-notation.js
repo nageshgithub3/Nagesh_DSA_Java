3fd20cb8e22811ca61e7b2cc69a059bebc4e5eb7
Rangaswamy Gari Nagesh <142003241+nageshgithub3@users.noreply.github.com>
2026-03-04 10:06:43 +0530

    Time: 23 ms (15.19%) | Memory: 60.5 MB (16.28%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(t) {
+
+    let stack = [];
+
+    for(let i=0;i<t.length;i++){
+        if(t[i] === "+"||t[i] === "/"||t[i] === "*"||t[i] === "-"){
+
+            let a = stack.pop();
+            let b = stack.pop();
+
+            let ans = eval(`${b} ${t[i]} ${a}`);
+            stack.push(Math.trunc(ans));
+            
+        }else{
+            stack.push(t[i]);
+        }
+    }
+    return Number(stack.pop());
+    
+};
