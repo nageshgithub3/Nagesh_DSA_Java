3618b8d42acfd8382608ca38d6015e06fa208a10
Rangaswamy Gari Nagesh <142003241+nageshgithub3@users.noreply.github.com>
2026-03-04 10:16:52 +0530

    Time: 6 ms (57.74%) | Memory: 55.4 MB (96.51%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -6,14 +6,21 @@ var evalRPN = function(t) {
 
     let stack = [];
 
+    const map = {
+        "+" : (a,b) => (b+a),
+        "-" : (a,b) => (b-a),
+        "*" : (a,b) => (b*a),
+        "/" : (a,b) => Math.trunc(b/a)
+    }
+
     for(let i=0;i<t.length;i++){
-        if(["+","-","/","*"].includes(t[i])){
+        if(map[t[i]]){
 
             let a = stack.pop();
             let b = stack.pop();
 
-            let ans = eval(`${b} ${t[i]} ${a}`);
-            stack.push(Math.trunc(ans));
+            let ans = map[t[i]](Number(a),Number(b));
+            stack.push(ans);
             
         }else{
             stack.push(t[i]);
