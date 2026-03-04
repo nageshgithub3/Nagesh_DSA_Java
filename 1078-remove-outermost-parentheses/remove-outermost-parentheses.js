03724dc2e5e533bb5b47f853107c071af01795fb
Rangaswamy Gari Nagesh <142003241+nageshgithub3@users.noreply.github.com>
2026-03-04 08:24:32 +0530

    Time: 0 ms (100.00%) | Memory: 55.9 MB (79.81%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.js
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.js
@@ -4,15 +4,15 @@
  */
 var removeOuterParentheses = function(s) {
 
-    let level = 0
+    let level = -1;
     let ans = "";
     for(let i=0;i<s.length;i++){
         if(s[i]==="("){
             ++level;
-            ans += ((level > 1)? s[i] : "");
+            ans += ((level)? s[i] : "");
             
         }else{
-            ans += ((level > 1)? s[i] : "");
+            ans += ((level)? s[i] : "");
             --level;
         }
     }
