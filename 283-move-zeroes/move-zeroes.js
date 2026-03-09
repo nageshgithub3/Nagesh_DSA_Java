f1f1bb412995ee987e1cc25f5ddecdad7ea9f21c
Rangaswamy Gari Nagesh <142003241+nageshgithub3@users.noreply.github.com>
2026-03-09 15:17:13 +0530

    Time: 0 ms (100.00%) | Memory: 60.8 MB (32.79%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -4,16 +4,16 @@
  */
 var moveZeroes = function(nums) {
 
-    let x = 0;
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]!==0){
-            nums[x]=nums[i];
-            x++;
-        }
-    }
+        let n = nums.length;
+        let left = 0;
 
-    for(let i = x; i<nums.length;i++){
-        nums[i]=0;
-    }
+        for(let right = 0; right < n; right++){
+            if(nums[right]!=0){
+                let temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
+            }
+        }
     
 };
