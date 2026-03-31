109825cd1ca0a93dfc22f82379c92dd7d43e9aba
Rangaswamy Gari Nagesh <142003241+nageshgithub3@users.noreply.github.com>
2026-03-31 15:10:53 +0530

    Time: 1 ms (94.97%) | Memory: 64.7 MB (87.88%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -9,15 +9,15 @@ var maxProfit = function(prices) {
     let min = prices[0];
     let maxProfit = 0;
 
-    for(let i=1;i<prices.length;i++){
+    for(let i=0;i<prices.length;i++){
         if(prices[i]-min > maxProfit){
             maxProfit = prices[i]-min;
         }
+
         if(prices[i] < min){
             min = prices[i];
         }
     }
     return maxProfit;
-
     
 };
